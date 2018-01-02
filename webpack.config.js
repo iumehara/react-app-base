7a6ba25b5ff947bf36a0d01e0d577dd3d7776206
iumehara <iumehara@users.noreply.github.com>
2018-01-02 16:46:48 +0900

    removes unneeded entry from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/js/index.js', './test/App.test.js'],
+  entry: ['./src/js/index.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
